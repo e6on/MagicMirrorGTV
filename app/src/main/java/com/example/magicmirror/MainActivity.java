adfa1ff0f3fe5f514f5e114c8bfafaa6e6d61dc3
Egon Padjus <egonipost@gmail.com>
2023-05-09 16:38:58 +0300

    Done

--- app/src/main/java/com/example/magicmirror/MainActivity.java
+++ app/src/main/java/com/example/magicmirror/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.magicmirror;
 
 import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 import androidx.fragment.app.FragmentActivity;
+
+
 
 /*
  * Main Activity class that loads {@link MainFragment}.
@@ -17,11 +23,27 @@ public class MainActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ProgressBar progressBar = findViewById(R.id.progressBar2);
         WebView webView = findViewById(R.id.webView);
+
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NORMAL);
         webView.getSettings().setUseWideViewPort(true);
         webView.getSettings().setLoadWithOverviewMode(true);
+        // Set up the WebView
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                // Show the progress bar
+                progressBar.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                // Hide the progress bar
+                progressBar.setVisibility(View.GONE);
+            }
+        });
         webView.loadUrl("http://152.67.65.3:8080/");
     }
 
---
+++ app/src/main/java/com/example/magicmirror/SplashScreenActivity.java
@@ -0,0 +1,27 @@
+package com.example.magicmirror;
+
+import androidx.fragment.app.FragmentActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+public class SplashScreenActivity extends FragmentActivity {
+    private static final int SPLASH_SCREEN_TIMEOUT = 3000; // 3 seconds
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        // Use a handler to delay launching the main activity
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_SCREEN_TIMEOUT);
+    }
+}
